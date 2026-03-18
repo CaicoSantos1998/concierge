f0aff6f97946a55616a08994bd79f34b70f9d7ef
CS <caico_fc@hotmail.com>
2026-03-18 15:35:31 -0300

    The new exception handling has been added to the globalHandler class

--- src/main/java/github/caicosantos/concierge/exceptions/GlobalExceptionHandler.java
+++ src/main/java/github/caicosantos/concierge/exceptions/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package github.caicosantos.concierge.exceptions;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -19,10 +21,22 @@ public class GlobalExceptionHandler {
         return ErrorResponse.conflict(e.getMessage());
     }
 
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse handlerDataIntegrityViolationException(DataIntegrityViolationException e) {
+        return new ErrorResponse(HttpStatus.CONFLICT.value(), "This a duplicate key that violates unique constraint!", List.of());
+    }
+
     @ExceptionHandler(AuthorizationDeniedException.class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public ErrorResponse handleAccessDenied(AuthorizationDeniedException e) {
-        return new ErrorResponse(HttpStatus.FORBIDDEN.value(), "Access denied!", List.of());
+        return new ErrorResponse(HttpStatus.FORBIDDEN.value(), "Authorization denied!", List.of());
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ErrorResponse handlerAccessDeniedException(AccessDeniedException e) {
+        return new ErrorResponse(HttpStatus.FORBIDDEN.value(), "Access Denied!", List.of());
     }
 
     @ExceptionHandler(SearchCombinationNotFoundException.class)
